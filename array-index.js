276f4cce1827d05b7df24e10ed5890c8b453cb0a
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 00:39:15 +0600

    final push

--- array-index.js
+++ array-index.js
@@ -1,6 +1,9 @@
 var bookPages = ['hablu', 'programming', 'chouddogusthi', 'battery', 'gymnasium'];
 var batteryIndex = bookPages.indexOf('chouddogusthi');
 console.log(batteryIndex);
+
 var bookNames = ['biology', 'physics', 'chemistry', 'highermath', 'generalmath', 'religion'];
 var highermathIndex = bookNames.indexOf('highermath');
 console.log(highermathIndex);
+var index = bookNames[3];
+console.log(index);
