c006bc592546fb8ba63ecbdfc20009159526482e
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 00:32:43 +0600

    index finding

---
+++ array-index.js
@@ -0,0 +1,6 @@
+var bookPages = ['hablu', 'programming', 'chouddogusthi', 'battery', 'gymnasium'];
+var batteryIndex = bookPages.indexOf('chouddogusthi');
+console.log(batteryIndex);
+var bookNames = ['biology', 'physics', 'chemistry', 'highermath', 'generalmath', 'religion'];
+var highermathIndex = bookNames.indexOf('highermath');
+console.log(highermathIndex);
--- array.js
+++ array.js
@@ -1,2 +1,12 @@
 var friendsAge = [11, 12, 15, 16];
-console.log(friendsAge);
+// console.log(friendsAge);
+var muriChanachurFee = [221, 34, 23, 54];
+// console.log(muriChanachurFee);
+
+var actressName = ['Mahi', 'Opu', 'Alia'];
+console.log(actressName);
+console.log(actressName.length);
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
+console.log(vowels.length);
