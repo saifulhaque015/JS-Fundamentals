8cf56f452e18d0ada9f85a8ff69a9cd81dcdf71d
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 01:02:03 +0600

    push pop practice

---
+++ push-pop.js
@@ -0,0 +1,8 @@
+var lastBench = ['abir', 'balam', 'salam'];
+console.log(lastBench);
+lastBench.push('kalam');
+console.log(lastBench);
+lastBench.pop();
+lastBench.pop();
+lastBench.pop();
+console.log(lastBench);
